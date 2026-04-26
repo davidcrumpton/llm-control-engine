9c6706a575ff80c10943b832a01ff9e9bf3d085c
david <azcrumpty@msn.com>
2026-04-26 12:59:32 -0600

    refactor: remove legacy plugin system and example implementations

--- plugins/examples/src/plugins/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @module plugins
- * Barrel exports for the hook plugin system.
- */
-export { HOOK_EVENTS, HookPriority, } from "./types.js";
-export { HookManager } from "./hook-manager.js";
-export { PluginLoader } from "./plugin-loader.js";
-export { EngineHookIntegration, } from "./engine-hooks.js";
